cd8e7fa6c499f7bfa98ba740acc5e4a6be6fede4
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-01-18 21:10:47 +0900

    swagger更新

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::Auth::SessionsController < ApplicationController
   def index
     if current_api_v1_user
-      render json: { is_login: true, data: current_api_v1_user }
+      render json: { is_login: true, data: current_api_v1_user.slice(:id, :email, :name) }, status: :ok
     else
-      render json: { is_login: false, message: "ユーザーが存在しません" }
+      render json: { is_login: false, message: "ユーザーが存在しません" }, status: :unauthorized
     end
   end
 end
